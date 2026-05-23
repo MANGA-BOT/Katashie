b690121733a219a2dba9b239e2c6edb07d06a111
Indah Mahendra Marpaung <rigomanga4@gmail.com>
2026-05-23 03:40:07 +0100

    Update ping command message format

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,4 @@
 // commands/ping.js
-
 import fs from 'fs';
 import path from 'path';
 import { sendStyled } from '../events/messageHandler.js';
@@ -7,12 +6,7 @@ import { sendStyled } from '../events/messageHandler.js';
 export async function pingCommand(message, client) {
     const msgTimestamp = message.messageTimestamp * 1000;
     const latency = Date.now() - msgTimestamp;
-
-    const text = `👾 *ｋａｔａｓｈｉｅ* : ${latency} ms`;
-
-    await sendStyled(
-        message.key.remoteJid,
-        text,
-        client
-    );
+    
+    const text = `⚡ *Speed* : ${latency} ms`;
+    await sendStyled(message.key.remoteJid, text, client);
 }
