0cb25b9f79b5643842bc2ebd17114f84708fd478
Indah Mahendra Marpaung <rigomanga4@gmail.com>
2026-05-23 08:55:06 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,12 +1,19 @@
 // commands/ping.js
+
 import fs from 'fs';
 import path from 'path';
 import { sendStyled } from '../events/messageHandler.js';
 
 export async function pingCommand(message, client) {
-    const msgTimestamp = message.messageTimestamp * 1000;
-    const latency = Date.now() - msgTimestamp;
-    
-    const text = `⚡ *Speed* : ${latency} ms`;
-    await sendStyled(message.key.remoteJid, text, client);
+const msgTimestamp = message.messageTimestamp * 1000;
+const latency = Date.now() - msgTimestamp;
+
+const text = `⚡ *Speed* : ${latency} ms`;
+
+await sendStyled(
+    message.key.remoteJid,
+    text,
+    client
+);
+
 }
