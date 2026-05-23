173b5c4b31c3c2d604ac8acc6f6520c0e45bf8a3
Indah Mahendra Marpaung <rigomanga4@gmail.com>
2026-05-23 09:05:21 +0100

    Refactor ping command for improved error handling

--- commands/ping.js
+++ commands/ping.js
@@ -1,19 +1,19 @@
-// commands/ping.js
-
-import fs from 'fs';
-import path from 'path';
 import { sendStyled } from '../events/messageHandler.js';
 
 export async function pingCommand(message, client) {
-const msgTimestamp = message.messageTimestamp * 1000;
-const latency = Date.now() - msgTimestamp;
+    try {
+        const msgTimestamp = Number(message.messageTimestamp) * 1000;
+        const latency = Date.now() - msgTimestamp;
 
-const text = `⚡ *Speed* : ${latency} ms`;
+        const text = `👾 *KATASHIE* : ${latency} ms`;
 
-await sendStyled(
-    message.key.remoteJid,
-    text,
-    client
-);
+        await sendStyled(
+            message.key.remoteJid,
+            text,
+            client
+        );
 
+    } catch (err) {
+        console.error('Erreur commande ping :', err);
+    }
 }
